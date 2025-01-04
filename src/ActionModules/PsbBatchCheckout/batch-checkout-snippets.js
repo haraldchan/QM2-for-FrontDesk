f91fa75eef2fd85afdf126cc823d85e412f790dc
haraldchan <katahane42@gmail.com>
2025-01-04 22:57:58 +0800

    updated

--- src/ActionModules/PsbBatchCheckout/batch-checkout-snippets.js
+++ src/ActionModules/PsbBatchCheckout/batch-checkout-snippets.js
@@ -1,61 +1,62 @@
-/**
- * TODO:
- * 3. if no query result, continue
- */
-
-/**
- * Find the span element by innerText.
- * @param {string} innerText The innerText of the target span.
- * @returns {HTMLSpanElement}
- */
 function findSpan(label) {
 	return Array.from(document.querySelectorAll('span')).find((span) => span.innerText === label)
 }
 
+// sequence funcs
+async function executeSequence(room) {
+	await delay(0, roomQuery, room)
+	await delay(2000, clickCheckout)
+	await delay(2000, clickOk)
+}
+
+function delay(ms, func, ...args) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			console.log(`Executing: ${func.name}`)
+			func(...args)
+			resolve()
+		}, ms)
+	})
+}
+
+function roomQuery(room) {
+	queryInput.value = room
+	queryInput.dispatchEvent(change)
+	queryBtn.click()
+	console.log('roomQuery executed')
+}
+
+function clickCheckout() {
+	sortBtn.click()
+	sortBtn.click()
+	coBtn = findSpan('退房')
+	coBtn.click()
+	console.log('clickCheckout executed')
+}
+
+function clickOk() {
+	let okBtn = Array.from(document.querySelector('.el-message-box__btns').querySelectorAll('span')).find((span) => span.innerText === '确定')
+	okBtn.click()
+	console.log('clickOk executed')
+}
+
+// initializing
 const rooms = []
 const queryInput = document.querySelector('input[placeholder="请输入查询条件"]')
 const queryBtn = findSpan('查 询')
 const sortBtn = document.querySelectorAll('.cell')[7]
-
 const change = new Event('input', {
 	bubbles: true,
 	cancelable: true,
 })
 
-// check out flow
-queryInput.value = rooms[0]
-queryInput.dispatchEvent(change)
-queryBtn.click()
-sortBtn.click()
-sortBtn.click()
-
-let okBtn, cxlBtn
-let coBtn = findSpan('退房')
-coBtn.click()
-
-setTimeout(() => {
-	okBtn = Array.from(document.querySelector('.el-message-box__btns').querySelectorAll('span')).find((span) => span.innerText === '确定')
-	cxlBtn = Array.from(document.querySelector('.el-message-box__btns').querySelectorAll('span')).find((span) => span.innerText === '取消')
-	cxlBtn.click()
-}, 500)
-
-function loop(room) {
-	setTimeout(() => {
-		queryInput.value = room
-		queryInput.dispatchEvent(change)
-		queryBtn.click()
-	}, 1000)
-
-	setTimeout(() => {
-		coBtn = findSpan('退房')
-		coBtn.click()
-	}, 2000)
-
-	setTimeout(() => {
-		okBtn.click()
-	}, 4000)
-}
-
-for (let i = 0; i < array.length; i++) {
-	loop(rooms[i])
-}
+let index = 0
+const loop  = setInterval(() => {
+	if (index > rooms.length) {
+		clearInterval(loop)
+		return
+	}
+	console.log('Starting sequence...')
+	executeSequence(rooms[index])
+	index++
+}, 10000) 
